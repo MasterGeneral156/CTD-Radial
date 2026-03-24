e3d641c1c18b5fc1632354fec0d126d5c2aaa37c
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-03-24 14:33:12 -0400

    Update CTDRadial.java

--- ctd-radial-neo/src/main/java/mastergeneral156/chasethedragon/radial/CTDRadial.java
+++ ctd-radial-neo/src/main/java/mastergeneral156/chasethedragon/radial/CTDRadial.java
@@ -33,7 +33,6 @@ import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
 
 @Mod(CTDRadial.MODID)
 public class CTDRadial {
